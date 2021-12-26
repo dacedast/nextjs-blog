948dc415b3f3be4c8b7a61c19036e6476585a9e9
dacedast <ryudast@gmail>
2021-12-26 14:13:09 +0100

    bug fixing

--- services/index.js
+++ services/index.js
@@ -39,6 +39,7 @@ export const getPosts = async () => {
   return result.postsConnection.edges;
 };
 
+
 export const getRecentPosts = async () => {
   const query = gql`
     query GetPostDetails() {
