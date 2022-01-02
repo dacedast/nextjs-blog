1304d4bbd6bb749735900d52dfd82b5d24c056f9
dacedast <ryudast@gmail>
2022-01-02 14:39:22 +0100

    burger icon fixed1

--- components/Burger.jsx
+++ components/Burger.jsx
@@ -4,7 +4,7 @@ const Burger = ({ isOpen, toggle}) => {
     return (
         <div className='burger' onClick={toggle}>
             <span className={isOpen ? 'burger1 rotate-45 ' : 'rotate-0 burger1'}></span>
-            <span className={isOpen ? ' burger2 translate-x-5 opacity-0' : 'translate-x-50 burger2 opacity-100'}></span>
+            <span className={isOpen ? ' burger2 translate-x-5 opacity-0' : ' burger2 opacity-100'}></span>
             <span className={isOpen ? ' burger3 -rotate-45 ' : 'rotate-0 burger3'}></span>
         </div>
     )
