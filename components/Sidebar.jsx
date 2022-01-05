907732b7b711b818da82d841a37275b6ad3eff60
dacedast <ryudast@gmail>
2022-01-05 19:32:42 +0100

    styles changed

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -9,7 +9,7 @@ const Sidebar = ({isOpen, toggle}) => {
     }, []);
     
     return (
-        <div className={isOpen ? 'flex flex-col p-4 bg-white text-lg sidebar' : 'hidden'} onClick={toggle}>
+        <div className={isOpen ? 'flex flex-col p-4 bg-black text-white text-lg sidebar' : 'hidden'} onClick={toggle}>
           {categories.map((category) => (
             <Link key={category.slug} href={`/category/${category.slug}`} onClick={toggle} isOpen={isOpen}>
                 {category.name}
